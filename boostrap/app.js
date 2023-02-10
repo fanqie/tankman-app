a511dd0ebbf0d4a866c6ac5c2024e161d03b33ba
nick <272584437@qq.com>
2023-02-10 14:42:28 +0800

    feat:init app

---
+++ boostrap/app.js
@@ -0,0 +1,4 @@
+const {Application} = require("tankman/freamwork/")
+app = new Application()
+app.boot()
+module.exports = app
---
+++ main.js
@@ -0,0 +1,6 @@
+const {Engine,Facades} = require("../tankman.js/freamwork/")
+const boostrap = require("./boostrap/app")
+const engine = new Engine(boostrap)
+
+
+engine.StartUp()
