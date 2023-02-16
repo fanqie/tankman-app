13cef5ea1c271be6a894b1fe502ee5a7c370660e
nick <272584437@qq.com>
2023-02-16 10:59:54 +0800

    feat:Added default config  of  the cluster module

--- Config/App.js
+++ Config/App.js
@@ -2,7 +2,15 @@ const DatabaseProvider = require("tankman/framework/provider/DatabaseProvider")
 const RouteProvider = require("tankman/framework/provider/RouteProvider")
 const LogProvider = require("tankman/framework/provider/LogProvider")
 module.exports = {
-    "app": {
+    /**
+     * Clusters of Tankman.js processes can be used to run multiple instances of http-server that can distribute workloads among their application threads.
+     * The cluster module allows easy creation of child processes that all share server ports.
+     */
+    cluster:{
+        enabled:true,
+        process_max_count:8
+    },
+    app: {
         providers: [
             DatabaseProvider,
             RouteProvider,
