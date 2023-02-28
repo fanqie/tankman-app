1a262c05c20263a20015fa206a4779221b55b3a2
nick <272584437@qq.com>
2023-02-28 11:21:23 +0800

    feat:add  cache as file system

---
+++ config/Cache.js
@@ -0,0 +1,99 @@
+const Facades = require("tankman/framework/facades/Facades");
+module.exports = {
+    "cache": {
+        default: Facades.Env.Get("CACHE_DRIVE", "file"),
+        //mysql for MySQL or MariaDB
+        file: {
+            saveFile: Facades.Env.Get("CACHE_DRIVE", ".runtime/cache.json")
+        },
+        redis: {
+            client: 'mysql',
+            connection: {
+                port: Facades.Env.Get("DB_PORT", 3306),
+                host: Facades.Env.Get("DB_HOST", ""),
+                user: Facades.Env.Get("DB_USERNAME", ""),
+                database: Facades.Env.Get("DB_DATABASE", ""),
+                password: Facades.Env.Get("DB_PASSWORD", ""),
+            },
+            pool: {
+                min: 2,
+                max: 10,
+            },
+            acquireConnectionTimeout: 10000,
+
+        },
+        //pg for PostgreSQL, CockroachDB and Amazon Redshift, pg-native for PostgreSQL with native C++ bindings (requires PostgresSQL installed to link against)
+        pg: {
+            client: 'pg',
+            connection: {
+                port: Facades.Env.Get("DB_PORT", 5432),
+                host: Facades.Env.Get("DB_HOST", ""),
+                user: Facades.Env.Get("DB_USERNAME", ""),
+                database: Facades.Env.Get("DB_DATABASE", ""),
+                password: Facades.Env.Get("DB_PASSWORD", ""),
+            },
+            pool: {
+                max: 5,
+                min: 0,
+            },
+            acquireConnectionTimeout: 10000,
+            migrations: {
+                tableName: 'migrations'
+            }
+        },
+        //tedious for MSSQL(Sql Server)
+        mssql: {
+            client: 'mssql',
+            connection: {
+                port: Number(Facades.Env.Get("DB_PORT", 1433)),
+                server: Facades.Env.Get("DB_HOST", "."),
+                user: Facades.Env.Get("DB_USERNAME", "sa"),
+                database: Facades.Env.Get("DB_DATABASE", ""),
+                password: Facades.Env.Get("DB_PASSWORD", ""),
+                driver: "msnodesqlv8",
+                options: {
+                    trustedConnection: true,
+
+                }
+            },
+
+            pool: {
+                max: 5,
+                min: 0,
+            },
+            acquireConnectionTimeout: 10000,
+            migrations: {
+                tableName: 'migrations'
+            }
+        },
+        //oracledb for Oracle Database
+        oracledb: {
+            client: 'oracledb',
+            connection: {
+                port: Facades.Env.Get("DB_PORT", 1521),
+                host: Facades.Env.Get("DB_HOST", ""),
+                user: Facades.Env.Get("DB_USERNAME", ""),
+                database: Facades.Env.Get("DB_DATABASE", ""),
+                password: Facades.Env.Get("DB_PASSWORD", ""),
+            },
+            pool: {
+                max: 5,
+                min: 0,
+            },
+            acquireConnectionTimeout: 10000,
+            migrations: {
+                tableName: 'migrations'
+            }
+
+        },
+        //sqlite3 for SQLite3
+        sqlite3: {
+            client: 'sqlite3',
+            connection: {
+                filename: "file:memDb1?mode=memory&cache=shared",
+                flags: ['OPEN_URI', 'OPEN_SHAREDCACHE']
+            }
+        },
+
+    },
+}
