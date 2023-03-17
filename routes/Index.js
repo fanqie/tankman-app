e2a077486a868c1530a1487d054b329bc6f20eb2
nick <272584437@qq.com>
2023-03-17 15:21:28 +0800

    feat:Adjust router input parameters

--- routes/Index.js
+++ routes/Index.js
@@ -1,21 +1,22 @@
 const Facades = require("tankman/framework/facades/Facades");
 const HelloController = require("../app/http/controller/HelloController");
-helloController = Facades.App.Singleton(HelloController)
+
 module.exports = () => {
     Facades.Route.Get("/", (httpCtx) => {
-        httpCtx.response.SetBody('<h1>home page!</h1>');
+        httpCtx.response.Html("home page");
     });
-    Facades.Route.Get("/dbList", helloController, "List");
+    Facades.Route.Get("/dbList", [HelloController, "List"]);
     Facades.Route.Post("/list/:id", (httpCtx, id) => {
         httpCtx.response.SetBody(`<h1>home page!</h1>
 <p>${httpCtx.request.Post("value")}</p>
 <p>${httpCtx.request.Get("id")}</p>
 `);
     });
-    Facades.Route.Post("/test/:id", helloController, "Index").Middleware(['test1', 'test2']);
+
+    Facades.Route.Post("/test/:id", [HelloController, "Index"]).Middleware(['test1', 'test2']);
 
     Facades.Route.Get("/404-page", (httpCtx) => {
-        httpCtx.response.SetBody('<h1>NotFound Page,http-Code:404!</h1>');
+        httpCtx.response.Html("404 Not Found Page");
     }).Name("404");
 
 
